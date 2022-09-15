1fcc85bde59a5f25b6912a4fa57b6191ceb3c016
Israa Ahmed El-nabty <israaelnabty@gmail.com>
2022-09-15 09:02:30 +0200

    feat: Add Main File

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * OnDemand Traffic Light Control - FWD.c
+ *
+ * Created: 8/31/2022 4:53:53 PM
+ * Author : Israa
+ */ 
+
+#include "APP/app.h"
+
+int main(void)
+{
+	APP_init();
+	APP_start();
+}
